04b20978c7c056dc0415882dec092f3c6b76b7b2
Pascal Bugnion <pascal@bugnion.org>
2018-01-12 14:55:13 +0000

    Add missing file

---
+++ sml_sync/screens/base.py
@@ -0,0 +1,13 @@
+
+from prompt_toolkit.key_binding.key_bindings import KeyBindings
+
+
+class BaseScreen(object):
+
+    def __init__(self):
+        self.main_container = None
+        self.bindings = KeyBindings()
+        self.use_default_bindings = True
+
+    def on_mount(self, application):
+        pass
