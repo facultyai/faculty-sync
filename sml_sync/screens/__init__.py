2eb89d38235ead3bca969d81b53d8a2f81ba83c8
Pascal Bugnion <pascal@bugnion.org>
2018-01-14 08:52:21 +0000

    Better layout of synchronizing screen

--- sml_sync/screens/__init__.py
+++ sml_sync/screens/__init__.py
@@ -1,6 +1,7 @@
 
 from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.layout.containers import Window
+from prompt_toolkit.layout import HSplit
 
 from .diff import DifferencesScreen  # noqa
 from .walking_trees import WalkingFileTreesScreen, WalkingFileTreesStatus  # noqa
@@ -14,5 +15,7 @@ class SynchronizationScreen(BaseScreen):
 
     def __init__(self):
         super().__init__()
-        self.main_container = Window(
-            FormattedTextControl('Synchronizing'))
+        self.main_container = HSplit([
+            Window(height=1),
+            Window(FormattedTextControl('  Synchronizing'), height=1)
+        ])
