1dca81576c914a7b8def45239a701be13037c721
Pascal Bugnion <pascal@bugnion.org>
2018-01-16 07:47:52 +0000

    Correct version number

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@ from distutils.core import setup
 
 setup(
     name='sml_sync',
-    version='0.1.0',
+    version='0.1.0-dev',
     description='SherlockML file synchronizer',
     author='The SherlockML team',
     entry_points={
         'console_scripts': ['sml-sync=sml_sync:run']
     },
+    install_requires=[
+        'sml',
+        'daiquiri',
+        'paramiko',
+        'inflect',
+        'watchdog',
+        # This is currently missing prompt-toolkit (waiting for 2.0 to be released)
+    ]
 )
