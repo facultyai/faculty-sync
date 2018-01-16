4e0f6dc9dd5cf324d240bf2e61eb51d78149e301
Pascal Bugnion <pascal@bugnion.org>
2018-01-16 08:14:34 +0000

    Bump to version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='sml_sync',
-    version='0.1.1-dev',
+    version='0.1.1',
     description='SherlockML file synchronizer',
     author='The SherlockML team',
     packages=['sml_sync'],
