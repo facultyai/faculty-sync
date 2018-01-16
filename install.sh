ae05a8e453259890e6242faef5bed41b975a1c36
Pascal Bugnion <pascal@bugnion.org>
2018-01-16 08:07:04 +0000

    Bump version number to 0.1.0

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 pip install git+https://github.com/jonathanslenders/python-prompt-toolkit@2.0
-pip install https://bitbucket.org/theasi/sml-sync/get/0.1.0-rc2.zip
+pip install https://bitbucket.org/theasi/sml-sync/get/0.1.0.zip
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='sml_sync',
-    version='0.1.0-rc2',
+    version='0.1.0',
     description='SherlockML file synchronizer',
     author='The SherlockML team',
     entry_points={
