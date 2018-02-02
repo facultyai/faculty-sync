527b10d9a1720e4e07972d049fbefa3183cebbf1
Pascal Bugnion <pascal@bugnion.org>
2018-02-02 07:19:20 +0000

    Bump to vesion 0.1.4.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='sml_sync',
-    version='0.1.4-rc1',
+    version='0.1.4',
     description='SherlockML file synchronizer',
     author='The SherlockML team',
     packages=['sml_sync', 'sml_sync.screens'],
