aff0327c556f43d76e92729a5f1fa9fc7eda1469
Pascal Bugnion <pascal@bugnion.org>
2018-02-04 11:13:57 +0000

    Catch error removing non-existent file

--- sml_sync/sync.py
+++ sml_sync/sync.py
@@ -65,7 +65,11 @@ class Synchronizer(object):
         self._sftp.mkdir(os.path.join(self.remote_dir, path))
 
     def rmfile_remote(self, path):
-        self._sftp.remove(os.path.join(self.remote_dir, path))
+        logging.info('Removing remote file {}.'.format(path))
+        try:
+            self._sftp.remove(os.path.join(self.remote_dir, path))
+        except IOError:
+            logging.info('Remote file {} did not exist on remote.'.format(path))
 
     def rmdir_remote(self, path):
         self._sftp.rmdir(os.path.join(self.remote_dir, path))
