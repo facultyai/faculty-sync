c4516317b86fc7fa3b4c169d983ec65a5fb71511
Pascal Bugnion <pascal@bugnion.org>
2018-02-24 07:44:45 +0000

    Add version check at startup.

--- sml_sync/app.py
+++ sml_sync/app.py
@@ -7,6 +7,7 @@ from .controller import Controller
 from .pubsub import PubSubExchange
 from .ssh import get_ssh_details
 from .ui import View
+from .update import check_for_new_release
 
 
 def run():
@@ -23,6 +24,8 @@ def run():
 
     logging.info(
         'sml-sync started with configuration {}'.format(configuration))
+
+    check_for_new_release()
 
     exchange = PubSubExchange()
     exchange.start()
