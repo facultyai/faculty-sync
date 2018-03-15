c68936ac1e8e1e4fe878a5bbbe119f492f5f1821
janfreyberg <jan.freyberg@gmail.com>
2018-03-15 21:40:06 +0000

    Raising an error if config for a folder specified in two places.

--- sml_sync/config.py
+++ sml_sync/config.py
@@ -19,16 +19,39 @@ def get_config(directory: str) -> configparser.ConfigParser:
             s.strip() for s in string.split(delim)
         ]}
     )
-    config.read([project_conf_file, user_conf_file])
 
-    # "normalise" the paths to avoid issues with symlinks and ~
-    config.read_dict({
-        str(Path(key).expanduser().resolve()).rstrip('/'): value
-        for key, value in config.items()
-        if key.lower() != 'default'
-        and not config.has_section(str(Path(key).expanduser()
-                                       .resolve()).rstrip('/'))
-    })
+    if user_conf_file.exists():
+        # read the user conf file
+        config.read(user_conf_file)
+
+        # "normalise" the paths to avoid issues with symlinks and ~
+        config.read_dict({
+            str(Path(key).expanduser().resolve()).rstrip('/'): value
+            for key, value in config.items()
+            if key.lower() != 'default'
+            and not config.has_section(str(Path(key).expanduser()
+                                           .resolve()).rstrip('/'))
+        })
+
+    if project_conf_file.exists():
+        project_config = configparser.ConfigParser(
+            converters={'list': lambda string, delim=',': [
+                s.strip() for s in string.split(delim)
+            ]}
+        )
+        # read the project conf file
+        project_config.read([project_conf_file])
+        if len(project_config.sections()) > 1:
+            raise ValueError('The project config file is ambiguous, as it has '
+                             'more than two sections.')
+        elif len(project_config.sections()) == 1:
+            if str(directory) in config:
+                raise ValueError('You can\'t specify configurations for a '
+                                 'project in both the home and project '
+                                 'directory.')
+            config.read_dict({
+                str(directory): project_config[project_config.sections()[0]]
+            })
 
     if str(directory) in config:
         return config[str(directory)]
