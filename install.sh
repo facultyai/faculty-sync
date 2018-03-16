59953e4bcba10ea15e072227ec60dcd4d9a2162e
Pascal Bugnion <pascal@bugnion.org>
2018-03-16 07:10:43 +0000

    First release candidate for version 0.2.0.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 pip install --upgrade git+https://github.com/jonathanslenders/python-prompt-toolkit@2.0
-pip install https://bitbucket.org/theasi/sml-sync/get/0.1.7.zip
+pip install https://bitbucket.org/theasi/sml-sync/get/0.2.0-rc1.zip
--- sml_sync/version.py
+++ sml_sync/version.py
@@ -1,3 +1,3 @@
 
-__version__ = '0.1.7'
+__version__ = '0.2.0-rc1'
 version = __version__
