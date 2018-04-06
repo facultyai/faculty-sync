21b8f87e00a7aae70408f77789721ba3490e6bae
janfreyberg <jan.freyberg@gmail.com>
2018-04-06 15:23:09 +0100

    Version bump 0.22 -> 0.23

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 pip install --upgrade git+https://github.com/jonathanslenders/python-prompt-toolkit@2.0
-pip install https://bitbucket.org/theasi/sml-sync/get/0.2.2.zip
+pip install https://bitbucket.org/theasi/sml-sync/get/0.2.3-rc1.zip
