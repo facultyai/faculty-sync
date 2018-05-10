504e8655c4db10d75b2fb0feae88e05d4c5f1818
janfreyberg <jan.freyberg@gmail.com>
2018-05-10 12:33:08 +0100

    Remove + from string concatenation

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ setup(
         'console_scripts': ['sml-sync=sml_sync:run']
     },
     # prompt_toolkit 2.0 is currently only available from github:
-    dependency_links = ['https://github.com/'
-                        + 'jonathanslenders/python-prompt-toolkit/'
-                        + 'tarball/2.0#egg=prompt_toolkit-2.0'],
+    dependency_links=['https://github.com/'
+                      'jonathanslenders/python-prompt-toolkit/'
+                      'tarball/2.0#egg=prompt_toolkit-2.0'],
     install_requires=[
         'sml',
         'daiquiri',
