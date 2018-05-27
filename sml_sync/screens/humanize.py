c37496d98c8bdfea5f5157cfadbd96b9fed5bdb0
Pascal Bugnion <pascal@bugnion.org>
2018-05-27 14:55:29 +0100

    Human friendly information for attributes

--- sml_sync/screens/humanize.py
+++ sml_sync/screens/humanize.py
@@ -123,3 +123,29 @@ def naturaltime(value, future=False, months=True):
         return "now"
 
     return ago % delta
+
+
+suffixes = ('kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
+
+
+def naturalsize(value, format='%.1f'):
+    """Format a number of byteslike a human readable filesize (eg. 10 kB).  By
+    default, decimal suffixes (kB, MB) are used.  Passing binary=true will use
+    binary suffixes (KiB, MiB) are used and the base will be 2**10 instead of
+    10**3.  If ``gnu`` is True, the binary argument is ignored and GNU-style
+    (ls -sh style) prefixes are used (K, M) with the 2**10 definition.
+    Non-gnu modes are compatible with jinja2's ``filesizeformat`` filter."""
+
+    base = 1024
+    bytes = float(value)
+
+    if bytes == 1:
+        return '1 Byte'
+    elif bytes < base:
+        return '%d Bytes' % bytes
+
+    for i, s in enumerate(suffixes):
+        unit = base ** (i+2)
+        if bytes < unit:
+            return (format + ' %s') % ((base * bytes / unit), s)
+    return (format + ' %s') % ((base * bytes / unit), s)
