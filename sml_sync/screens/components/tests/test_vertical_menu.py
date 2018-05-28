970856efea309806cb594e91cf941edabcef50ce
Pascal Bugnion <pascal@bugnion.org>
2018-05-28 12:21:07 +0100

    Factor out common test fixtures

--- sml_sync/screens/components/tests/test_vertical_menu.py
+++ sml_sync/screens/components/tests/test_vertical_menu.py
@@ -5,13 +5,28 @@ from prompt_toolkit.key_binding.key_processor import KeyProcessor, KeyPress
 from .. import VerticalMenu, MenuEntry
 
 
+def get_menu_text(menu):
+    """ Get the formatted text corresponding to the menu """
+    control = to_container(menu).content
+    return control.text
+
+
+def simulate_key(menu, key):
+    """ Simulate passing `key` to a menu """
+    control = to_container(menu).content
+    key_bindings = control.key_bindings
+    key_processor = KeyProcessor(key_bindings)
+    key_processor.feed(KeyPress(key))
+    key_processor.process_keys()
+
+
 def test_simple_menu():
     entry1 = MenuEntry(1, 'menu entry 1')
     entry2 = MenuEntry(2, 'menu entry 2')
 
     menu = VerticalMenu([entry1, entry2])
 
-    menu_text = to_container(menu).content.text
+    menu_text = get_menu_text(menu)
     assert len(menu_text) == 2
     [menu_line1, menu_line2] = menu_text
     assert menu_line1 == ('reverse', 'menu entry 1\n')
@@ -24,14 +39,9 @@ def test_change_entry():
 
     menu = VerticalMenu([entry1, entry2])
 
-    control = to_container(menu).content
-    key_bindings = control.key_bindings
-    key_processor = KeyProcessor(key_bindings)
-    key_processor.feed(KeyPress('up'))
-    key_processor.process_keys()
+    simulate_key(menu, 'up')
 
-    menu_text = to_container(menu).content.text
-
+    menu_text = get_menu_text(menu)
     assert len(menu_text) == 2
     [menu_line1, menu_line2] = menu_text
     assert menu_line1 == ('', 'menu entry 1\n')
