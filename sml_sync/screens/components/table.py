a6f9017f04dd36d8953eaa559b582597c69b54ae
Pascal Bugnion <pascal@bugnion.org>
2018-05-30 07:50:12 +0100

    Ability to specify how columns should be aligned in table

--- sml_sync/screens/components/table.py
+++ sml_sync/screens/components/table.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 import itertools
-from typing import List
+from enum import Enum
+from typing import List, Optional
 
 from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.document import Document
@@ -8,7 +9,28 @@ from prompt_toolkit.layout import (
     Window, HSplit, FormattedTextControl, BufferControl, ScrollbarMargin)
 
 
-TableColumn = namedtuple('Column', ['rows', 'header'])
+class Alignment(Enum):
+    RIGHT = 'RIGHT'
+    LEFT = 'LEFT'
+
+
+class ColumnSettings(object):
+
+    def __init__(self, alignment=Alignment.LEFT):
+        self.alignment = alignment
+
+
+class TableColumn(
+        namedtuple('Column', ['rows', 'header', 'settings'])):
+
+    def __new__(
+            cls, rows: List[str],
+            header: str,
+            settings: Optional[ColumnSettings] = None
+    ):
+        if settings is None:
+            settings = ColumnSettings()
+        return super(TableColumn, cls).__new__(cls, rows, header, settings)
 
 
 class Table(object):
@@ -26,7 +48,10 @@ class Table(object):
                     len(column.header),
                     max((len(row) for row in column.rows), default=0)
             )
-            formatted_rows = [row.ljust(width, ' ') for row in column.rows]
+            formatted_rows = [
+                self._format_cell(row, column.settings, width)
+                for row in column.rows
+            ]
             formatted_headers.append(column.header.ljust(width, ' '))
             formatted_columns.append(formatted_rows)
 
@@ -53,6 +78,12 @@ class Table(object):
         self.window = HSplit(
             self._header_windows(formatted_headers) + body_windows
         )
+
+    def _format_cell(self, content, column_settings, width):
+        if column_settings.alignment == Alignment.LEFT:
+            return content.ljust(width)
+        else:
+            return content.rjust(width)
 
     def _header_windows(self, formatted_headers):
         if len(formatted_headers):
